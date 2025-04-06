b04adf044c408c23be55c348ed60005f96c2d4ad
isa-lago <isa.desarrollo.web@gmail.com>
2025-04-06 13:07:42 +0200

    Actualización de la home con secciones dinámicas

--- wp-content/themes/prueba-asdrubal-isabel-lago/includes/internal-section.php
+++ wp-content/themes/prueba-asdrubal-isabel-lago/includes/internal-section.php
@@ -2,12 +2,12 @@
     <div class="internal-container">
         <!-- Div izquierdo con imagen -->
         <div class="left">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/img-section-interna.webp" alt="Imagen Interna">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/img-section-interna.webp" alt="Imagen Interna">
         </div>
         <!-- Div derecho con h2, descripción y botón -->
         <div class="right">
-            <h2>Nuestro título de sección</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt.</p>
+            <h2>Esta sección cambia el contenido en función de la hora</h2>
+            <p id="hora-parrafo"></p>
             <a href="https://asdrubalseo.com/" class="cta-button">Llama a la acción</a>
         </div>
     </div>
--- wp-content/themes/prueba-asdrubal-isabel-lago/script.js
+++ wp-content/themes/prueba-asdrubal-isabel-lago/script.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Cambiar el título en el h1 de otra sección
     const h1 = document.getElementById("main-title");
     if (h1) {
-        h1.textContent = "Título cambiado por JavaScript";
+        h1.textContent = "Este Título lo hemos modificado con JS";
+    }
+
+    // Cambiar el texto del párrafo según la hora
+    var horaActual = new Date().getHours();
+    var parrafo = document.getElementById("hora-parrafo");
+
+    if (parrafo) {
+        if (horaActual < 16) {
+            parrafo.textContent = "Aún no son las 16:00 h, estás en horario de oficina, puedes llamarnos o si lo prefieres visita nuestra web";
+        } else {
+            parrafo.textContent = "Estamos fuera de horario de oficina, nuestro horario es hasta las 16:00 h, visita nuestra web!";
+        }
     }
 });
